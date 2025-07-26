cb828c43ae08d7b0af7893473adf8aed7c57be0c
fanyicharllson <fanyicharllson1@gmail.com>
2025-07-26 11:57:52 -0700

    Added wipe component to remove resume from puter, kv

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,7 @@ export default [
     route('/auth', "routes/auth.tsx"),
     route("/upload", 'routes/upload.tsx'),
     route("/resume/:id", 'routes/resume.tsx'),
+    route("/wipe", 'routes/wipe.tsx'),
     // Add a catch-all route to handle paths like "/.well-known/appspecific/com.chrome.devtools.json"
     route('*', "routes/catch-all.tsx"),
 ] satisfies RouteConfig;
